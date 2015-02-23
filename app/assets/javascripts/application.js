8cdcb0ef341c15c798e83fcdda10dffa42a440bc
C Haden <chaden@spoonflower.com>
2015-02-23 17:56:01 -0500

    javascript magic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,32 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+
+
+// When the user clicks on the save button, disable the button so that it cannot
+// be accidentally double-clicked.
+function disableButton() {
+  document.getElementById("btn").style.display = 'none';
+};
+
+// Hide the last empty row of the grading scale section when the page loads.
+// When the "Add Grade Threshold" button is clicked, show the last row. You do not
+// need to make it show an additional row if the user clicks again.
+function toggleEmpty( displayStyle ){
+  if(document.getElementById("btn btn-danger flush-association") != "null"){
+    document.getElementById("btn btn-danger flush-association").parentNode.parentNode.style.display= displayStyle;
+  };
+};
+
+// When a Delete button is clicked, hide the row to which it belongs, but mark the
+// hidden _destroy field in that row as truthy.
+function hideDeleted( deleteButton ){
+  deleteButton.parentNode.parentNode.parentNode.style.display = 'none';
+};
+
+// Back on the course detail page, you'll notice that clicking on the various nav
+// buttons in the blue box will cause the page to snap to that section. Implement
+// smooth scrolling instead. You'll have to look it up on Google.
+function smoothScrolling(){
+
+};
